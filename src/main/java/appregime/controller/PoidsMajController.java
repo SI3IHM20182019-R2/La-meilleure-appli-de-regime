5a8729cdbab6278ffed31bc06885fb685bad0165
Romain Giuntini <romain.giuntini@etu.univ-cotedazur.fr>
2019-03-10 13:46:21 +0100

    Correction du popup bloquant lors de la mise a jour du poids

--- src/main/java/appregime/controller/PoidsMajController.java
+++ src/main/java/appregime/controller/PoidsMajController.java
@@ -1,8 +1,16 @@
 package appregime.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class PoidsMajController extends Controller {
+
+    @FXML
+    private Button cancelButton;
+
     public PoidsMajController() {
         super("/appregime/view/poids_maj.fxml");
+        cancelButton.setOnAction(event -> myStage.close());
     }
 
 }
