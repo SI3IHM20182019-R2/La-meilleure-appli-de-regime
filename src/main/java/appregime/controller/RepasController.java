a5213864a44240c93a02ade3c9169f0acd52b23d
Unknown <donelia.monin@etu.unice.fr>
2019-03-10 01:51:14 +0100

    Amelioration affichage ListView

--- src/main/java/appregime/controller/RepasController.java
+++ src/main/java/appregime/controller/RepasController.java
@@ -12,6 +12,18 @@ public class RepasController extends Controller {
     private Label nomIngredient;
 
     @FXML
+    private Label lipides;
+
+    @FXML
+    private Label glucides;
+
+    @FXML
+    private Label proteines;
+
+    @FXML
+    private Label calories;
+
+    @FXML
     private ImageView imageIngredient;
 
     public RepasController() {
@@ -22,6 +34,10 @@ public class RepasController extends Controller {
     public void setRepas(PlatModel plat) {
         System.out.println(plat.getImagePath().getValue());
         nomIngredient.setText(plat.getName().getValue());
+        proteines.setText(String.valueOf(plat.getProteines().getValue()));
+        glucides.setText(String.valueOf(plat.getGlucides().getValue()));
+        lipides.setText(String.valueOf(plat.getLipides().getValue()));
+        calories.setText(String.valueOf(plat.getCalories().getValue()));
         imageIngredient.setImage(new Image(plat.getImagePath().getValue()));
     }
 }
