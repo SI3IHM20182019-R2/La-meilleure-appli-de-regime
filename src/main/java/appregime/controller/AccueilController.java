f434efa0bc79f397ccb40263e7b049d7864d4c6d
Unknown <donelia.monin@etu.unice.fr>
2019-03-10 23:55:51 +0100

    Ajout du calendrier page d'accueil

--- src/main/java/appregime/controller/AccueilController.java
+++ src/main/java/appregime/controller/AccueilController.java
@@ -4,18 +4,28 @@ import appregime.model.IngredientQuantiteModel;
 import appregime.model.ListPlats;
 import appregime.model.PlatModel;
 import appregime.model.UserModel;
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.util.Callback;
 
 import javax.swing.text.html.ImageView;
 import java.awt.*;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+
+
+
+import java.util.Date;
 
 public class AccueilController extends Controller {
 
@@ -52,6 +62,9 @@ public class AccueilController extends Controller {
     @FXML
     private ListView<PlatModel> platModelListView;
 
+    @FXML
+    private VBox calendar;
+
     private UserModel user;
 
 
@@ -66,11 +79,30 @@ public class AccueilController extends Controller {
         ListPlats l = new ListPlats();
         platModelListView.setItems(l.getPlatList());
         this.initRepasList();
+        this.initCalendar();
         this.PoidsActuel.setText(String.valueOf(this.user.getPoids()));
         this.regime.setText(this.user.getRegimes());
         this.objectif.setText(this.user.getObjectifvise());
         actualiser.setOnAction(event -> creerRegime());
 
+    }
+
+    public void initCalendar() {
+       BorderPane root = new BorderPane();
+      //  Scene scene = new Scene(root, 400, 400);
+
+       // this.myStage.s
+      //  scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+       // CalendarView calendarView = new CalendarView();
+      //  VBox test = new VBox();
+        DatePickerSkin datePickerSkin = new DatePickerSkin(new DatePicker(LocalDate.now()));
+        Node popupContent = datePickerSkin.getPopupContent();
+       // calendar.getChildren().addAll(popupContent);
+       root.setCenter(popupContent);
+       calendar.getChildren().addAll(root);
+
+      //  primaryStage.setScene(scene);
+       // primaryStage.show();
     }
 
     public void initRepasList() {
