46b85a8e03eda85f6b622895ee12fed66149efb5
Unknown <donelia.monin@etu.unice.fr>
2019-03-11 01:22:59 +0100

    Affichage des repas à l'accueil

--- src/main/java/appregime/model/RepasList.java
+++ src/main/java/appregime/model/RepasList.java
@@ -1,6 +1,7 @@
 package appregime.model;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class RepasList {
-    /*
+
 
     private ObservableMap<String, RepasModel> RepasMap;
 
@@ -17,16 +18,28 @@ public class RepasList {
         });
         ListPlats platList = new ListPlats();
         ObservableMap<String, PlatModel> mapPlat = platList.getPlatMap();
-        List<PlatModel> listPlat1 = new ArrayList<PlatModel>() {{
-            add(mapPlat.get("taboulet")), add(mapPlat.get("saladeCesar")), add(mapPlat.get("hamburgerFrites")}};
-        List<PlatModel> listPlat2 = {mapPlat.get("saladeDeFruits"), mapPlat.get("saladeCesar")};
-        List<PlatModel> listPlat3 = {mapPlat.get("soupeTomate"), mapPlat.get("hamburgerFrites"), mapPlat.get("mousseAuChocolat")};
-        List<PlatModel> listPlat4 = {mapPlat.get("taboulet"), mapPlat.get("saladeCesar"), mapPlat.get("mousseAuChocolat")};
-        RepasMap.put("Repas1"  , new RepasModel("Repas1", "c'est le premier ", "../resources/appregime/images/imageRepas/Repas1.jpg",listPlat1 ));
-        RepasMap.put("Repas2"  , new RepasModel("Repas2", "c'est le deuxième ", "../resources/appregime/images/imageRepas/Repas2.jpg",listPlat2));
-        RepasMap.put("Repas3"  , new RepasModel("Repas3", "c'est le troisème ", "../resources/appregime/images/imageRepas/Repas3.jpg",listPlat3 ));
-        RepasMap.put("Repas4"  , new RepasModel("Repas4", "c'est le quatrième ","../resources/appregime/images/imageRepas/Repas4.jpg" ,listPlat4));
+
+
+        List<PlatModel> listPlat1 = new ArrayList<PlatModel>()
+        {{ add(mapPlat.get("taboulet")); add(mapPlat.get("saladeCesar")); add(mapPlat.get("hamburgerFrites"));}};
+        List<PlatModel> listPlat2 = new ArrayList<PlatModel>() { {add(mapPlat.get("saladeDeFruits"));add(mapPlat.get("saladeCesar")); add(mapPlat.get("mousseAuChocolat"));}};
+        List<PlatModel> listPlat3 = new ArrayList<PlatModel>() { {add(mapPlat.get("soupeTomate")); add(mapPlat.get("hamburgerFrites")); add(mapPlat.get("mousseAuChocolat"));}};
+        List<PlatModel> listPlat4 = new ArrayList<PlatModel>() { {add(mapPlat.get("taboulet")); add(mapPlat.get("saladeCesar")); add(mapPlat.get("mousseAuChocolat"));}};
+        RepasMap.put("Repas1"  , new RepasModel("Repas1", "c'est le premier ", "appregime/images/imageRepas/Plat1.jpg",listPlat1 ));
+        RepasMap.put("Repas2"  , new RepasModel("Repas2", "c'est le deuxième ", "appregime/images/imageRepas/Plat2.jpg",listPlat2));
+        RepasMap.put("Repas3"  , new RepasModel("Repas3", "c'est le troisème ", "appregime/images/imageRepas/Plat3.jpg",listPlat3 ));
+        RepasMap.put("Repas4"  , new RepasModel("Repas4", "c'est le quatrième ","appregime/images/imageRepas/Plat4.jpg" ,listPlat4));
     }
 
-    */
+    public ObservableMap<String, RepasModel> getPlatMap() {
+        return RepasMap;
+    }
+
+
+    public ObservableList<RepasModel> getRepasList() {
+        ObservableList<RepasModel> l = FXCollections.observableList(new ArrayList<>());
+        l.addAll(RepasMap.values());
+        return l;
+    }
+
 }
