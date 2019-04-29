432e37b89cad3cb0b108e1629ae22f39fd4dd546
Remy Larroye <remy.larroye@etu.unice.fr>
2019-04-29 16:31:55 +0200

    Ajout de la recyclerView dans la page Mes objectifs

--- Android/Regime_App/app/src/main/java/com/example/regime_app/MenuFragments/ObjectifsFragment.java
+++ Android/Regime_App/app/src/main/java/com/example/regime_app/MenuFragments/ObjectifsFragment.java
@@ -1,53 +1,51 @@
 package com.example.regime_app.MenuFragments;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.regime_app.R;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
 import java.util.ArrayList;
 
 public class ObjectifsFragment extends Fragment {
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<Integer> mImageUrls = new ArrayList<>();
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.objectifs, container, false);
-        ArrayList<String> mNames = new ArrayList<>();
-        ArrayList<String> mImageUrls = new ArrayList<>();
-        getImages(view, mNames, mImageUrls);
 
-        return view;
-    }
-
-    private void getImages(View view, ArrayList mNames, ArrayList mImageUrls){
-
-        mImageUrls.add("@drawable/prise_muscle");
+        mImageUrls.add(R.drawable.prise_muscle);
         mNames.add("Prise de muscle");
 
-        mImageUrls.add("@drawable/amelioration_silhouette");
+        mImageUrls.add(R.drawable.amelioration_silhouette);
         mNames.add("Amélioration silhouette");
 
-        mImageUrls.add("@drawable/perte_de_graisse_localise");
-        mNames.add("Perte de graisse localisée");
+        mImageUrls.add(R.drawable.perte_de_graisse_localise);
+        mNames.add("Perte de grasse localisé");
 
-        mImageUrls.add("@drawable/reduire_les_glucides");
-        mNames.add("Réduire les glucides");
-
-
-        mImageUrls.add("@drawable/perte_de_poids");
+        mImageUrls.add(R.drawable.perte_de_poids);
         mNames.add("Perte de poids");
 
-        mImageUrls.add("@drawable/manger_plus_sain");
+        mImageUrls.add(R.drawable.reduire_les_glucides);
+        mNames.add("Réduire les glucides");
+
+        mImageUrls.add(R.drawable.manger_plus_sain);
         mNames.add("Manger plus sain");
 
-        initRecyclerView(view, mNames, mImageUrls);
+        initRecyclerView(view);
+        return view;
 
     }
 
-    private void initRecyclerView(View view, ArrayList mNames, ArrayList mImageUrls){
+    private void initRecyclerView(View view){
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(layoutManager);
@@ -55,8 +53,3 @@ public class ObjectifsFragment extends Fragment {
         recyclerView.setAdapter(adapter);
     }
 }
-
-
-
-
-
--- Android/Regime_App/app/src/main/java/com/example/regime_app/MenuFragments/RecyclerViewAdapter.java
+++ Android/Regime_App/app/src/main/java/com/example/regime_app/MenuFragments/RecyclerViewAdapter.java
@@ -1,29 +1,30 @@
 package com.example.regime_app.MenuFragments;
+
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.bumptech.glide.Glide;
 import com.example.regime_app.R;
+
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+/**
+ * Created by User on 2/12/2018.
+ */
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
-    private static final String TAG = "RecyclerViewAdapter";
-
-    //vars
     private ArrayList<String> mNames = new ArrayList<>();
-    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<Integer> mImageUrls = new ArrayList<>();
     private Context mContext;
 
-    public RecyclerViewAdapter(Context context, ArrayList<String> names, ArrayList<String> imageUrls) {
+    public RecyclerViewAdapter(Context context, ArrayList<String> names, ArrayList<Integer> imageUrls) {
         mNames = names;
         mImageUrls = imageUrls;
         mContext = context;
@@ -37,17 +38,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        Glide.with(mContext)
-                .asBitmap()
-                .load(mImageUrls.get(position))
-                .into(holder.image);
 
+        int identifier = mImageUrls.get(position);
+        holder.image.setImageResource(identifier);
         holder.name.setText(mNames.get(position));
 
         holder.image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked on an image: " + mNames.get(position));
                 Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
             }
         });
