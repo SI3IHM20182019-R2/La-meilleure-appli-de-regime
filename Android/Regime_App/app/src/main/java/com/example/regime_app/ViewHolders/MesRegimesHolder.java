9f89a23806d04241778940fb58033c52f2ecc36b
SABRI Anass <anessabiri3@gmail.com>
2019-05-09 16:14:50 +0200

    #Mes régimes details régime et donner un avis (première version )

--- Android/Regime_App/app/src/main/java/com/example/regime_app/ViewHolders/MesRegimesHolder.java
+++ Android/Regime_App/app/src/main/java/com/example/regime_app/ViewHolders/MesRegimesHolder.java
@@ -1,6 +1,8 @@
 package com.example.regime_app.ViewHolders;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -8,8 +10,12 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.regime_app.Adapters.MesRegimesAdapter;
+import com.example.regime_app.DetailsRegimeActivity;
 import com.example.regime_app.Models.Regime;
 import com.example.regime_app.R;
+
+import java.util.List;
 
 public class MesRegimesHolder extends RecyclerView.ViewHolder {
     private Context context;
@@ -18,6 +24,7 @@ public class MesRegimesHolder extends RecyclerView.ViewHolder {
     private TextView description ;
     private ImageView imageregime ;
     private ImageButton deleteimage ;
+    private TextView voirdetails ;
 
     public MesRegimesHolder(@NonNull View itemView, Context context) {
         super(itemView);
@@ -25,13 +32,29 @@ public class MesRegimesHolder extends RecyclerView.ViewHolder {
         description = (TextView)itemView.findViewById(R.id.description);
         imageregime = (ImageView) itemView.findViewById(R.id.imageregime);
         deleteimage = (ImageButton) itemView.findViewById(R.id.deleteimage);
+        voirdetails =(TextView)itemView.findViewById(R.id.Viewdetails);
         this.context = context;
     }
 
-    public void bindData(final Regime regime) {
+    public void bindData(final Regime regime , final List<Regime> regimes , final MesRegimesAdapter ad) {
         imageregime.setImageResource(context.getResources().getIdentifier(regime.getImageName(), "drawable", context.getPackageName()));
         title.setText(regime.getNom());
         description.setText(regime.getDescription());
+        deleteimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regimes.remove(regime );
+                ad.setnotification();
+            }
+        });
+        voirdetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context , DetailsRegimeActivity.class ) ;
+                intent.putExtra("regime" , regime) ;
+                context.startActivity(intent) ;
+            }
+        });
 
     }
 }
