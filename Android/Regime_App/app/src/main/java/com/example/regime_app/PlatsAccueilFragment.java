c90592248cfc44e69b1a79fac9039813c62cda39
Monin <donelia.monin@etu.unice.fr>
2019-05-13 03:15:48 +0200

    Correction du dernier commit

--- Android/Regime_App/app/src/main/java/com/example/regime_app/PlatsAccueilFragment.java
+++ Android/Regime_App/app/src/main/java/com/example/regime_app/PlatsAccueilFragment.java
@@ -9,9 +9,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.regime_app.Models.Utilisateur;
+
+import java.util.List;
 
 
 public class PlatsAccueilFragment extends Fragment {
